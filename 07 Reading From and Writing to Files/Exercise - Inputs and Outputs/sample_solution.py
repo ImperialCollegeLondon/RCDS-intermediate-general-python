82afe748a51f5a51360fcddf03b0316e52f13ea9
coolernato <cmcooling@gmail.com>
2022-08-24 13:10:04 +0100

    Renamed IO sample solution

---
+++ 07 Reading From and Writing to Files/Exercise - Inputs and Outputs/sample_solution.py
@@ -0,0 +1,26 @@
+from compound_summaries import count_atoms_in_compounds
+
+def get_compounds_from_file(path):
+    with open(path) as f:
+        lines = f.readlines()
+
+    print(lines)
+    if len(lines) > 0:
+        print(lines[0].split())
+    compounds = {line.split()[0]: float(line.split()[1]) for line in lines}
+
+    return(compounds)
+
+def get_atoms_from_file(path):
+    compounds = get_compounds_from_file(path)
+
+    atom_totals = count_atoms_in_compounds(compounds)
+
+    return(atom_totals)
+
+def write_atoms_from_file(input_path, output_path):
+    atom_totals = get_atoms_from_file(input_path)
+
+    with open(output_path, "w") as f:
+        for atom in atom_totals:
+            f.write("{} {}\n".format(atom, atom_totals[atom]))
