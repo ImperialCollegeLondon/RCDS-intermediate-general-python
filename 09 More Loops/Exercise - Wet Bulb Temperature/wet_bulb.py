f76c1f829901b9ba745367ffdb55d0c12d25fd8d
coolernato <cmcooling@gmail.com>
2022-10-20 16:06:07 +0100

    Completed wet bulb temperature exercise

--- 09 More Loops/Exercise - Wet Bulb Temperature/wet_bulb.py
+++ 09 More Loops/Exercise - Wet Bulb Temperature/wet_bulb.py
@@ -1,23 +1,30 @@
 from math import atan, sqrt
 
 def wet_bulb_stull(dry_temperature, relative_humidity):
-    # This function calcualtes the wet bulb temeprature in Celsius according to the Stull formula
-    # dry_temperature is the dry bulb temeprature in Celcius (e.g. it might have the value 20)
+    # This function calculates the wet bulb temperature in Celsius according to the Stull formula
+    # dry_temperature is the dry bulb temperature in Celsius (e.g. it might have the value 20)
     # relative humidity is the relative humidity in % (e.g. it might have the value 50)
 
-    # Sometimes when calcualting the result of a specific equation, it can be easier to split it into several smaler terms. This can increase readability and make finding errors easier, although it is slower to execute than usig a single long expression
+    # Sometimes when calculating the result of a specific equation, it can be easier to split it into several smaller terms. This can increase readability and make finding errors easier, although it is slower to execute than using a single long expression
     term_1 = dry_temperature * atan(0.151977 * (sqrt(relative_humidity + 8.313659)))
     term_2 = atan(dry_temperature + relative_humidity)
     term_3 = - atan(relative_humidity - 1.676331)
     term_4 = 0.00391838 * relative_humidity ** (3/2) * atan(0.023101 * relative_humidity)
     term_5 = - 4.686035
-    wet_temeprature =  term_1 + term_2  + term_3 + term_4 + term_5
+    wet_temperature =  term_1 + term_2  + term_3 + term_4 + term_5
 
-    return(wet_temeprature)
+    return(wet_temperature)
 
-def habitability_analyser (dry_temperatures, relatives_humidities):
-    for dry_temperature, relative_humidity in zip(dry_temperatures, relatives_humidities):
-        if wet_bulb_stull(dry_temperature, relative_humidity) > 35:
-            return(True)
+def habitability_analyser(dry_temperatures, relative_humidities):
+    for dry_temperature, relative_humidity in zip(dry_temperatures, relative_humidities):
+        if wet_bulb_stull(dry_temperature, relative_humidity) > 30:
+            return(False)
         
-    return(False)
+    return(True)
+
+def find_hot_indexes(dry_temperatures, relative_humidities):
+    wet_temperatures = tuple(wet_bulb_stull(dry_temperature, relative_humidity) for dry_temperature, relative_humidity in zip(dry_temperatures, relative_humidities))
+
+    hot_indexes = [index for index, wet_temperature in enumerate(wet_temperatures) if wet_temperature > 30]
+
+    return(hot_indexes)
--- 09 More Loops/Exercise - Wet Bulb Temperature/wet_bulb_checker.py
+++ 09 More Loops/Exercise - Wet Bulb Temperature/wet_bulb_checker.py
@@ -15,12 +15,12 @@ def check_web_bulb_stull(dry_temperature, relative_humidity, reference_wet_tempe
     try:
         wet_temperature = wet_bulb_stull(dry_temperature, relative_humidity)
     except:
-        print("When called with a temperature {}C and arelative humidity of {}%, wet_bulb_stull raised the followin exception. You may have modified he function - try re-downloading the file if this problem persists.".format(dry_temperature, relative_humidity))
+        print("When called with a temperature {}C and a relative humidity of {}%, wet_bulb_stull raised the following exception. You may have modified he function - try re-downloading the file if this problem persists.".format(dry_temperature, relative_humidity))
         raise
 
-    assert type(wet_temperature) == float, "When called with a temperature {}C and arelative humidity of {}%, wet_bulb_stull returned a vlaue which was of the type {} instead of a float. You may have modified he function - try re-downloading the file if this problem persists.".format(dry_temperature, relative_humidity, type(wet_temperature))
+    assert type(wet_temperature) == float, "When called with a temperature {}C and a relative humidity of {}%, wet_bulb_stull returned a value which was of the type {} instead of a float. You may have modified he function - try re-downloading the file if this problem persists.".format(dry_temperature, relative_humidity, type(wet_temperature))
 
-    assert abs(wet_temperature - reference_wet_temperature) < 0.1, "When called with a temperature {}C and arelative humidity of {}%, wet_bulb_stull returned {}C as a wet bulb temperature, insead of te crrect value of {}C. You may have modified he function - try re-downloading the file if this problem persists.".format(dry_temperature, relative_humidity, wet_temperature, reference_wet_temperature)
+    assert abs(wet_temperature - reference_wet_temperature) < 0.1, "When called with a temperature {}C and a relative humidity of {}%, wet_bulb_stull returned {}C as a wet bulb temperature, instead of te correct value of {}C. You may have modified he function - try re-downloading the file if this problem persists.".format(dry_temperature, relative_humidity, wet_temperature, reference_wet_temperature)
 
 check_web_bulb_stull(10, 50, 5.101)
 check_web_bulb_stull(35, 99, 34.96)
@@ -43,3 +43,53 @@ def habitability_analyser_checker(dry_temperatures, relative_humidities, referen
     except:
         print("When habitability_analyser was called with the dry temperatures of {} and relative humidities of {} the following exception was raised. Check the logic of your code.".format(dry_temperatures, relative_humidities))
         raise
+
+    assert type(student_result) == bool, "When habitability_analyser was called with the dry temperatures {} and relative humidities {} the returned result had a type of {} instead of bool. Check the logic of your code.".format(dry_temperatures, relative_humidities, type(student_result))
+
+    assert student_result == reference_result, "When habitability_analyser was called with the dry temperatures  {} and relative humidities {} the returned result a value of {} instead of the correct result of {}. Check the logic of your code.".format(dry_temperatures, relative_humidities, student_result, reference_result)
+
+# Check habitability_analyser returns the correct values
+habitability_analyser_checker((), (), True)
+habitability_analyser_checker((10,), (50,), True)
+habitability_analyser_checker((40,), (99,), False)
+habitability_analyser_checker((40, 10), (99, 50), False)
+habitability_analyser_checker((10, 40), (50, 99), False)
+habitability_analyser_checker((40, 50), (99, 90), False)
+habitability_analyser_checker((10, 35), (50, 50), True)
+habitability_analyser_checker((10, 40, 40), (50, 99, 10), False)
+
+print("Congratulations - your function 'habitability_analyser' passed all the tests")
+
+# Import find_hot_indexes
+try:
+    from wet_bulb import find_hot_indexes
+except ModuleNotFoundError:
+    assert False, "wet_bulb_checker.py tried to import from the file wet_bulb.py but the file didn't exist. Check it is present and the name is spelt correctly."
+except ImportError:
+    assert False, "wet_bulb_checker.py tried to import the function 'find_hot_indexes' from the file wet_bulb.py. The file existed, but the function didn't. Check the function is in that file and that the name is spelt correctly."
+
+# Check find_hot_indexes is a function
+assert type(find_hot_indexes) == FunctionType, "find_hot_indexes is not a function. Make sure you're using def to define it as a function and have not redefined it later in your script."
+
+def find_hot_indexes_checker(dry_temperatures, relative_humidities, reference_result):
+    try:
+        student_result = find_hot_indexes(dry_temperatures, relative_humidities)
+    except:
+        print("When find_hot_indexes was called with the dry temperatures of {} and relative humidities of {} the following exception was raised. Check the logic of your code.".format(dry_temperatures, relative_humidities))
+        raise
+
+    assert type(student_result) == list, "When find_hot_indexes was called with the dry temperatures {} and relative humidities {} the returned result had a type of {} instead of list. Check the logic of your code.".format(dry_temperatures, relative_humidities, type(student_result))
+
+    assert student_result == reference_result, "When find_hot_indexes was called with the dry temperatures  {} and relative humidities {} the returned result a value of {} instead of the correct result of {}. Check the logic of your code.".format(dry_temperatures, relative_humidities, student_result, reference_result)
+
+# Check find_hot_indexes_checker returns the correct values
+find_hot_indexes_checker((), (), [])
+find_hot_indexes_checker((10,), (50,), [])
+find_hot_indexes_checker((40,), (99,), [0])
+find_hot_indexes_checker((40, 10), (99, 50), [0])
+find_hot_indexes_checker((10, 40), (50, 99), [1])
+find_hot_indexes_checker((40, 50), (99, 90), [0,1])
+find_hot_indexes_checker((10, 35), (50, 50), [])
+find_hot_indexes_checker((10, 40, 40), (50, 99, 10), [1])
+
+print("Congratulations: both functions passed all the tests!")
